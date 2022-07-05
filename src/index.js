235ced7cf61b91ee4b7a06aa5000fb73a1bdf27f
NguyenKhanhDuy0112 <duynguyen.011202@gmail.com>
2022-07-05 08:42:46 +0700

    done

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const morgan = require('morgan')
 const cookieParser = require('cookie-parser')
 const db = require('./config/db')
 const dotenv = require('dotenv')
-
+const cors = require('cors')
 dotenv.config()
 
 const router = require('./routes')
@@ -12,6 +12,7 @@ const app = express()
 const PORT = process.env.PORT || 3000 
 
 app.use(morgan('common'))
+app.use(cors())
 
 app.use(cookieParser())
 
